efbafae58bc70f53259d499f81cb0daa9bba0140
nyeinchan17204 <92505220+nyeinchan17204@users.noreply.github.com>
2021-10-26 13:01:19 +0630

    Update download.py

--- bot/plugins/download.py
+++ bot/plugins/download.py
@@ -5,14 +5,20 @@ import lk21
 import requests
 import wget
 import urllib.parse
+import logging
+import tempfile
+import typing
+import ffmpeg
 from lk21.extractors.bypasser import Bypass
 from bs4 import BeautifulSoup
 from time import sleep
-from pyrogram import Client, filters
+from pyrogram import Client, filters, types
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from bot.helpers.sql_helper import gDriveDB, idsDB
 from bot.helpers.utils import CustomFilters, humanbytes
 from bot.helpers.downloader import download_file, utube_dl
 from bot.helpers.ythelper import convert_flac, sizeof_fmt, upload_hook, ytdl_download
+from utils import customize_logger
 from bot.helpers.gdrive_utils import GoogleDrive 
 from bot import DOWNLOAD_DIRECTORY, LOGGER
 from bot.config import Messages, BotCommands
@@ -33,20 +39,62 @@ def _download(client, message):
       msg = GoogleDrive(user_id).clone(link)
       sent_message.edit(msg)
     if 'youtu' in link:
-      link = message.text
-      LOGGER.info(f'YTDL:{user_id}: {link}')
-      sent_message.edit(Messages.DOWNLOADING.format(link))
-      result, file_path = utube_dl(link)
-      if result:
-        sent_message.edit(Messages.DOWNLOADED_SUCCESSFULLY.format(os.path.basename(file_path), humanbytes(os.path.getsize(file_path))))
-        msg = GoogleDrive(user_id).upload_file(file_path)
-        sent_message.edit(msg)
-        LOGGER.info(f'Deleteing: {file_path}')
-        os.remove(file_path)
-      else:
-        sent_message = message.reply_text('🕵️**PORNHUB ERROR**', quote=True)
-    
-    
+      # check remaining quota
+    chat_id = message.chat.id
+
+    if message.chat.type != "private" and not message.text.lower().startswith("/ytdl"):
+        logging.warning("%s, it's annoying me...🙄️ ", message.text)
+        return
+
+    url = re.sub(r'/ytdl\s*', '', message.text)
+    logging.info("start %s", url)
+
+    if not re.findall(r"^https?://", url.lower()):
+        Redis().update_metrics("bad_request")
+        message.reply_text("I think you should send me a link.", quote=True)
+        return
+    bot_msg: typing.Union["types.Message", "typing.Any"] = message.reply_text("Processing", quote=True)
+    client.send_chat_action(chat_id, 'upload_video')
+    temp_dir = tempfile.TemporaryDirectory()
+
+    result = ytdl_download(url, temp_dir.name, bot_msg)
+    logging.info("Download complete.")
+
+    markup = InlineKeyboardMarkup(
+        [
+            [  # First row
+                InlineKeyboardButton(  # Generates a callback query when pressed
+                    "audio",
+                    callback_data="audio"
+                )
+            ]
+        ]
+    )
+
+    if result["status"]:
+        client.send_chat_action(chat_id, 'upload_document')
+        video_paths = result["filepath"]
+        bot_msg.edit_text('Download complete. Sending now...')
+        for video_path in video_paths:
+            filename = pathlib.Path(video_path).name
+            remain = bot_text.remaining_quota_caption(chat_id)
+            size = sizeof_fmt(os.stat(video_path).st_size)
+            meta = get_metadata(video_path)
+            client.send_video(chat_id, video_path,
+                              supports_streaming=True,
+                              caption=f"`{filename}`\n\n{url}\n\nsize: {size}\n\n{remain}",
+                              progress=upload_hook, progress_args=(bot_msg,),
+                              reply_markup=markup,
+                              **meta
+                              )
+            Redis().update_metrics("video_success")
+        bot_msg.edit_text('Download success!✅')
+    else:
+        client.send_chat_action(chat_id, 'typing')
+        tb = result["error"][0:4000]
+        bot_msg.edit_text(f"Download failed!❌\n\n```{tb}```", disable_web_page_preview=True)
+
+    temp_dir.cleanup()
    
 
 @Client.on_message(filters.private & filters.incoming & (filters.document | filters.audio | filters.video | filters.photo) & CustomFilters.auth_users)
